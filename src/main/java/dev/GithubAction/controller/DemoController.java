4bac46036282a3fb2545c3e4317c80d1cdb6a86b
asher27 <asher1225@gmail.com>
2022-12-21 15:13:23 +0900

    cretae Demo Controller to check the server works well.

---
+++ src/main/java/dev/GithubAction/controller/DemoController.java
@@ -0,0 +1,13 @@
+package dev.GithubAction.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/demo")
+    public String demo() {
+        return "demo";
+    }
+}
